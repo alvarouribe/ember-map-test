18e616c40b9086319fc1625b88a723e28b166875
Alvaro Uribe <alvaro.uribe@vocus.co.nz>
2016-08-16 10:27:10 +1200

    removed id

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   myIcon: {
-    url: "/assets/images/logo.png"
-    // ,
-    // size: new google.maps.size(30,30),
-    // scaledSize: new google.maps.size(20,20),
-    // anchor: new google.maps.point(15, 15),
-    // origin: new google.maps.point(0, 0),
-    // labelOrigin: new google.maps.point(30, 15)
+    url: "/assets/images/logo.png",
+    size: new google.maps.size(30,30),
+    scaledSize: new google.maps.size(20,20),
+    anchor: new google.maps.point(15, 15),
+    origin: new google.maps.point(0, 0),
+    labelOrigin: new google.maps.point(30, 15)
   },
   actions: {
     handleClick() {
